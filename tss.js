58f9c40f4f96631d1265baa59b8832b8fdc6d0ed
William Mitre Filho <willmitre@gmail.com>
2026-02-21 16:16:45 -0300

    Moved script to root directory as standard for this so-called 'JumiLoader'

--- tss.js
+++ tss.js
@@ -1,22 +1,78 @@
-JumiLoader.scriptPath = "file:///home/mitre/tss/scripts/"
-
 JumiLoader.include('kbugol/kbugol')
 JumiLoader.include('soum/soum')
-JumiLoader.include('tss')
 
-Jumi.ready = ()=>{
+let A, TSS
 
-	TSS.start()
-	kbug = new KBugol()
-	kbug.listen()
+Jumi.ready = () => {
 
-	TSS.main.listenTo("letterKeyKPressed", ()=>{
-		console.log("I sense a key")
-	})
-	TSS.main.listenTo("keyTPressed", ()=>{
-		TSS.add("text").in(TSS.root)
-	})
-	TSS.main.listenTo("keyCPressed", ()=>{
-		TSS.add("circle").in(TSS.root)
-	})
+	class ThreeSimpleScripts{
+		static add(type, name){
+
+			if(!TSS[`${type}s`])
+				TSS[`${type}s`] = {}
+			let soum = A(type)
+
+			if(!name)
+				name = `${type}${TSS.numberOf(type)+1}`
+
+			TSS[`${type}s`][name]?.element.remove()
+			TSS[`${type}s`][name] = soum
+			return soum
+		}
+		static numberOf(type){
+			if(!TSS[`${type}s`])
+				return 0
+
+			return Object.getOwnPropertyNames(TSS[`${type}s`]).length
+		}
+		static start(){
+			let tss = new ThreeSimpleScripts()
+			tss.bind()
+			ThreeSimpleScripts.tss = tss
+			ThreeSimpleScripts.main = tss.main
+			ThreeSimpleScripts.root = tss.main.element
+		}
+
+		constructor(){
+			this.main = Soum.svg(330)
+		}
+		bind(){
+			this.main.appendTo(document.body)
+		}
+	}
+
+	class Group {
+		constructor(elements = []){
+			this.elements = elements
+		}
+		set x(x){
+			this._x = x
+			this.elements.forEach((element)=>{
+				element.x = x
+			})
+		}
+		set y(y){
+			this._y = y
+			this.elements.forEach((element)=>{
+				element.y = y
+			})
+		}
+	}
+	
+	SoumElement.prototype.listenTo = function(eventName, callback){
+		KBugol.addListener(eventName, this)
+		this[eventName] = callback
+	}
+
+	SoumElement.prototype.listenToSome = function(namedCallbacks){
+		for(var eventName in namedCallbacks)
+			this.listenTo(eventName, namedCallbacks[eventName])
+	}
+	SoumElement.prototype.stopListeningTo = function(eventName){
+		KBugol.removeListener(eventName, this)
+	}
+	A = Soum.a
+	TSS = ThreeSimpleScripts
+
+	JumiLoader.broadcast(Group)
 }
