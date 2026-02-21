92884b5860b0a022fb7f30fb509389fe17229a72
William Mitre Filho <willmitre@gmail.com>
2026-02-21 14:38:42 -0300

    Added all into this 'Jumi-Loader'-Compatible model of sorts

--- scripts/tss.js
+++ scripts/tss.js
@@ -1,18 +1,99 @@
-class ThreeSimpleScripts{
-	static start(){
-		let tss = new ThreeSimpleScripts()
-		tss.bind()
-		ThreeSimpleScripts.tss = tss
-		ThreeSimpleScripts.main = tss.main
-		ThreeSimpleScripts.root = tss.main.element
+JumiLoader.include('kbugol/kbugol')
+JumiLoader.include('soum/soum')
+
+let A, TSS
+
+Jumi.ready = () => {
+
+	class ThreeSimpleScripts{
+		static add(type, name){
+
+			if(!TSS[`${type}s`])
+				TSS[`${type}s`] = {}
+			let soum = A(type)
+
+			if(!name)
+				name = `${type}${TSS.numberOf(type)+1}`
+
+			TSS[`${type}s`][name]?.element.remove()
+			TSS[`${type}s`][name] = soum
+			return soum
+		}
+		static numberOf(type){
+			if(!TSS[`${type}s`])
+				return 0
+
+			return Object.getOwnPropertyNames(TSS[`${type}s`]).length
+		}
+		static start(){
+			let tss = new ThreeSimpleScripts()
+			tss.bind()
+			ThreeSimpleScripts.tss = tss
+			ThreeSimpleScripts.main = tss.main
+			ThreeSimpleScripts.root = tss.main.element
+		}
+
+		constructor(){
+			this.main = Soum.svg(330)
+		}
+		bind(){
+			this.main.appendTo(document.body)
+		}
 	}
-	constructor(){
-		this.main = Soum.svg(330)
+
+	class Group {
+		constructor(elements = []){
+			this.elements = elements
+		}
+		set x(x){
+			this._x = x
+			this.elements.forEach((element)=>{
+				element.x = x
+			})
+		}
+		set y(y){
+			this._y = y
+			this.elements.forEach((element)=>{
+				element.y = y
+			})
+		}
 	}
-	bind(){
-		this.main.appendTo(document.body)
+	
+	SoumElement.prototype.listenTo = function(eventName, callback){
+		KBugol.addListener(eventName, this)
+		this[eventName] = callback
 	}
+	SoumElement.prototype.stopListeningTo = function(eventName){
+		KBugol.removeListener(eventName, this)
+	}
+	SoumElement.prototype.keyESCAPEPressed = ()=>{
+		KBugol.target?.unfocus?.()
+	}
+	SoumElement.prototype.focus = function(){
+		Array.from(document.querySelector("svg *")).forEach((element)=>{
+			element.classList.remove("focused")
+		})
+		this.element.classList.add("focused")
+		KBugol.target = this
+	}
+	SoumElement.prototype.unfocus = function(){
+		this.element.classList.remove("focused")
+		KBugol.target = TSS.main
+	}
+	SoumGraphic.prototype.keyARROWUPPressed = function(){
+		this.y = Number(this.y) - 5
+	}
+	SoumGraphic.prototype.keyARROWDOWNPressed = function(){
+		this.y = Number(this.y) + 5
+	}
+	SoumGraphic.prototype.keyARROWLEFTPressed = function(){
+		this.x = Number(this.x) - 5
+	}
+	SoumGraphic.prototype.keyARROWRIGHTPressed = function(){
+		this.x = Number(this.x) + 5
+	}
+	A = Soum.a
+	TSS = ThreeSimpleScripts
+
+	JumiLoader.broadcast(Group)
 }
-
-const TSS = ThreeSimpleScripts
-
--- tss.js
+++ tss.js
@@ -1,69 +1,22 @@
 JumiLoader.scriptPath = "file:///home/mitre/tss/scripts/"
 
-let A, kbug
+JumiLoader.include('kbugol/kbugol')
+JumiLoader.include('soum/soum')
+JumiLoader.include('tss')
 
-GreediScript
-.needs('kbugol/kbugol')
-.and('soum/soum')
-.to(()=>{
-	init()
+Jumi.ready = ()=>{
+
+	TSS.start()
+	kbug = new KBugol()
+	kbug.listen()
+
 	TSS.main.listenTo("letterKeyKPressed", ()=>{
 		console.log("I sense a key")
 	})
 	TSS.main.listenTo("keyTPressed", ()=>{
-		TSS.add("text", "textOne").in(TSS.root).focus()
+		TSS.add("text").in(TSS.root).focus()
 	})
 	TSS.main.listenTo("keyCPressed", ()=>{
-		TSS.add("circle", "circleOne").in(TSS.root).focus()
+		TSS.add("circle").in(TSS.root).focus()
 	})
-})
-
-function init(){
-	TSS.start()
-	SoumElement.prototype.listenTo = function(eventName, callback){
-		KBugol.addListener(eventName, this)
-		this[eventName] = callback
-	}
-	SoumElement.prototype.stopListeningTo = function(eventName){
-		KBugol.removeListener(eventName, this)
-	}
-	SoumElement.prototype.keyESCAPEPressed = ()=>{
-		KBugol.target?.unfocus?.()
-	}
-	SoumElement.prototype.focus = function(){
-		Array.from(document.querySelector("svg *")).forEach((element)=>{
-			element.classList.remove("focused")
-		})
-		this.element.classList.add("focused")
-		KBugol.target = this
-	}
-	SoumElement.prototype.unfocus = function(){
-		this.element.classList.remove("focused")
-		KBugol.target = TSS.main
-	}
-	SoumGraphic.prototype.keyARROWUPPressed = function(){
-		this.y = Number(this.y) - 5
-	}
-	SoumGraphic.prototype.keyARROWDOWNPressed = function(){
-		this.y = Number(this.y) + 5
-	}
-	SoumGraphic.prototype.keyARROWLEFTPressed = function(){
-		this.x = Number(this.x) - 5
-	}
-	SoumGraphic.prototype.keyARROWRIGHTPressed = function(){
-		this.x = Number(this.x) + 5
-	}
-	TSS.add = (type, name)=>{
-		if(!TSS[`${type}s`])
-			TSS[`${type}s`] = {}
-		let soum = A(type)
-
-		
-		TSS[`${type}s`][name]?.element.remove()
-		TSS[`${type}s`][name] = soum
-		return soum
-	}
-	A = Soum.a
-	kbug = new KBugol()
-	kbug.listen()
 }
